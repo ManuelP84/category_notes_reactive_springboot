0953a247793ed99ccd94e52827fcd1fea14e2013
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-04 00:04:10 -0500

    Start category and notes project

---
+++ src/main/java/com/sofkau/categoryNotesReactive/CategoryNotesReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.categoryNotesReactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryNotesReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryNotesReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/categoryNotesReactive/CategoryNotesReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.categoryNotesReactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryNotesReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
