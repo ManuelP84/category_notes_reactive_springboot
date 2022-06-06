a636ce3575785871c17905948a08a37d9353840f
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-05 22:12:59 -0500

    CreateCategoryUseCae and CreateCategoryRoute implemented

---
+++ src/main/java/com/sofkau/categoryNotesReactive/routes/CreateCategoryRoute.java
@@ -0,0 +1,31 @@
+package com.sofkau.categoryNotesReactive.routes;
+
+import com.sofkau.categoryNotesReactive.model.CategoryDto;
+import com.sofkau.categoryNotesReactive.usecases.CreateCategoryUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class CreateCategoryRoute {
+
+    @Bean
+    public RouterFunction<ServerResponse> createCategory(CreateCategoryUseCase createCategoryUseCase){
+        return route(
+                POST("api/v1/create"),
+                request -> request.bodyToMono(CategoryDto.class)
+                        .flatMap(categoryDto -> createCategoryUseCase.apply(categoryDto))
+                        .flatMap(categoryDto -> ServerResponse.status(HttpStatus.CREATED)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(categoryDto))
+                        .onErrorResume(error -> ServerResponse.status(HttpStatus.BAD_REQUEST).build())
+        );
+
+    }
+}
---
+++ src/main/java/com/sofkau/categoryNotesReactive/usecases/CreateCategoryUseCase.java
@@ -0,0 +1,37 @@
+package com.sofkau.categoryNotesReactive.usecases;
+
+import com.sofkau.categoryNotesReactive.mapper.CategoryMapper;
+import com.sofkau.categoryNotesReactive.model.CategoryDto;
+import com.sofkau.categoryNotesReactive.repository.ICategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@Service
+@AllArgsConstructor
+public class CreateCategoryUseCase implements Function<CategoryDto, Mono<CategoryDto>> {
+
+    private final ICategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+
+    @Override
+    public Mono<CategoryDto> apply(CategoryDto categoryDto) {
+        return validateCategoryDto(categoryDto)
+                .flatMap(catDto -> categoryRepository.save(categoryMapper.convertDtoToEntity().apply(catDto)))
+                .map(category -> categoryMapper.convertEntityToDto().apply(category));
+    }
+
+    private Boolean validateCategoryProps(CategoryDto categoryDto){
+        return categoryDto.getId() != null && categoryDto.getTitle() != null;
+    }
+
+    private Mono<CategoryDto> validateCategoryDto(CategoryDto categoryDto) {
+        return validateCategoryProps(categoryDto)?
+                Mono.just(categoryDto)
+                :
+                Mono.error(() -> new Throwable("The category does not have all the necessary attributes."));
+    }
+}
